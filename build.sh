19838e7ba0a3a91eb1c31d3499ad82deaaef284b
Mikhail Grigorev <sleuthhound@gmail.com>
2014-12-15 09:52:08 +0500

    Fixed build.sh
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+rm -rf build
+rm -rf build-aux
+rm -rf autom4te.cache
+rm -f config.log
+rm -f config.status
+rm -f configure
+rm -f Makefile
+rm -f *.a
+rm -f *.in
+rm -f *.m4
+rm -f api/*.in
+rm -f app/*.in
+rm -f core/*.in
+rm -f netif/*.in
+rm -f include/*.in
+
 autoreconf -i
 PATH=/c/Espressif/xtensa-lx106-elf/bin/:$PATH
 mkdir build
